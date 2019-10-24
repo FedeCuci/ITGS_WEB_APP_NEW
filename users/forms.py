41b41470fc56ee58474b3f22649a14882575b00d
Fede <fede.cuci@outlook.com>
2019-10-24 03:47:51 -0700

    Removed choiced from users registration form and updated user model

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,35 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Choices
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    choices = forms.ModelMultipleChoiceField(queryset=Choices.objects.all(), required=True, widget=forms.CheckboxSelectMultiple)
+    student = forms.BooleanField(required=False)
+    teacher = forms.BooleanField(required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'choices', 'password1', 'password2']
+        fields = ['username', 'email', 'student','teacher', 'password1', 'password2']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        student = cleaned_data.get('student')
+        teacher = cleaned_data.get('teacher')
+        email = cleaned_data.get('email')
+
+        if not student and not teacher:
+            raise forms.ValidationError('You must be either a teacher or a studen')
+        if email:
+            if '@isutrecht.nl' not in email:
+                raise forms.ValidationError('You must be a member of ISU to sign up.')
+        if email and student:
+            if 'student' not in email:
+                raise forms.ValidationError('You must be a student to sign up as a student')
+        if email and teacher:
+            if 'student' in email:
+                raise forms.ValidationError('You must be a teacher to sign up as a teacher')
+
 
 
 class UserUpdateForm(forms.ModelForm):
--- users/models.py
+++ users/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-class Choices(models.Model):
-    description = models.CharField(max_length=300)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    choices = models.ManyToManyField(Choices)
+#    tag = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.username} Profile'
